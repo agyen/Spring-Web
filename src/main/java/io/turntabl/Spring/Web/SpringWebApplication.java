ad52f9dce64552cfb5d1da1037bed4c62724f5d0
agyen <izagyen96@gmail.com>
2019-11-20 16:50:35 +0000

    swagger

--- src/main/java/io/turntabl/Spring/Web/SpringWebApplication.java
+++ src/main/java/io/turntabl/Spring/Web/SpringWebApplication.java
@@ -4,10 +4,11 @@ import io.turntabl.Spring.Web.controller.Maths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SpringWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
--- src/main/java/io/turntabl/Spring/Web/controller/MathsController.java
+++ src/main/java/io/turntabl/Spring/Web/controller/MathsController.java
@@ -1,12 +1,14 @@
 package io.turntabl.Spring.Web.controller;
 
 import com.sun.media.sound.SF2InstrumentRegion;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class MathsController {
     @Autowired
